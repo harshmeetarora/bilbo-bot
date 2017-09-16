dcebbd8e508580c609686100b332b09571e9ae0c
maknoon <conniemob@gmail.com>
2017-09-16 06:32:02 -0400

    slackclient added

--- bilbobot.py
+++ bilbobot.py
@@ -1,11 +1,22 @@
 #!/bin/python
-
+import config
 from flask import Flask
+from slackclient import SlackClient
+
+sc = SlackClient(config.slack_token)
 app = Flask(__name__)
 
 @app.route('/')
 def hi():
     return "hi! i'm bilbo"
+
+@app.route('/post/<input>')
+def post(input):
+    sc.api_call(
+        "chat.postMessage",
+        channel="#general",
+        text=input
+    )
 
 
 if __name__ == '__main__':
