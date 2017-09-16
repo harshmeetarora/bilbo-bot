d75910d8088d3e8b8d458da7bd6e96bf5af355a5
maknoon <conniemob@gmail.com>
2017-09-16 12:41:27 -0400

    post a link of pic to bilbo works, + strings

--- bilbobot.py
+++ bilbobot.py
@@ -1,12 +1,37 @@
 #!/bin/python
-import config, json
-from flask import Flask
+import config, json, strings
+from flask import Flask, request
 from slackclient import SlackClient
 from watson_developer_cloud import VisualRecognitionV3
+
+'''
+MAIN BILBOBOT
+'''
 
 sc = SlackClient(config.slack_token)
 vr = VisualRecognitionV3('2017-09-16',api_key=config.watson_api_key)
 app = Flask(__name__)
+
+### === WATSON INTEGRATION ===
+
+@app.route('/doof', methods=['POST'])
+def watsonify():
+    # process image recognition
+    imgurl = request.form["text"]
+    res_body = json.dumps(vr.classify(images_url=imgurl), indent=2)
+    print(res_body)
+
+    # find the top 2 recognized keywords
+
+    sc.api_call(
+        "chat.postMessage",
+        channel="#general",
+        text=strings.watson_found)
+
+    return "200 OK"
+
+
+### === MAIN API ENDPOINTS ===
 
 @app.route('/')
 def hi():
---
+++ strings.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+### === GLOBAL STRINGS
+watson_found = 'image recognized as: {0}'
